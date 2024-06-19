c42f685d64de28cf1e49b376ccabe8f51ee9902c
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-06-20 01:02:26 +0530

    Unnecessary Imports

--- Backend/src/main/java/com/OOPS/bits_bids/DTO/ProductDTO.java
+++ Backend/src/main/java/com/OOPS/bits_bids/DTO/ProductDTO.java
@@ -1,9 +1,6 @@
 package com.OOPS.bits_bids.DTO;
 
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @Data
 public class ProductDTO {
--- Backend/src/main/java/com/OOPS/bits_bids/Entity/Bid.java
+++ Backend/src/main/java/com/OOPS/bits_bids/Entity/Bid.java
@@ -1,6 +1,5 @@
 package com.OOPS.bits_bids.Entity;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
